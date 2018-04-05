8111049c3deea94845d7f819e0a1756a1fe7efd5
Fellsmarch <37695784+Fellsmarch@users.noreply.github.com>
2018-04-05 16:51:27 +1200

    Changed name and removed 'this.' calls

--- oldStuff/ADCarryHero.java
+++ oldStuff/ADCarryHero.java
@@ -6,12 +6,12 @@
  * @version 0.1 04/04/2018
  */
 
-public class DamageDealer extends Hero
+public class ADCarryHero extends Hero
 	{
-		public DamageDealer() {
-			this.name = "AD Carry";
-			this.attackMod = 1.7;
-			this.health = 80;
-			this.defenseMod = 0.7;
+		public ADCarryHero() {
+			name = "AD Carry";
+			attackMod = 1.7;
+			health = 80;
+			defenseMod = 0.7;
 		}
 	}
