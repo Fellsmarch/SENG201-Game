cb4de598a067158a80c1a193a036153747b60a77
hannahregan <38155691+hannahregan@users.noreply.github.com>
2018-05-22 17:50:18 +1200

    Update HealingItem.java

--- items/HealingItem.java
+++ items/HealingItem.java
@@ -1,51 +1,38 @@
 package items;
 
+/**
+ * This class creates the class 'HealingItem' which is an object that can be stored in a Team's array list, healingInventory
+ * 
+ * @author Harrison Cook
+ * @author Hannah Regan
+ * @version 0.1 04/04/2018
+ */
+
 public class HealingItem extends Item {
 	
 	private int healingAmount; //Number of 25% heals to do
 	
-	// Constructor
+	/** 
+	 * Constructor -- HealingItem. Creates a healing item.
+	 * @param name The name of the healing item.
+	 * @param description The description of the item.
+	 * @param price How much the item costs to purchase.
+	 * @param healingAmount How much the item heals a hero.
+	 */
 	public HealingItem(String name, String description, int price, int healingAmount) {
 		super(name, description, price);
 		this.healingAmount = healingAmount;
 	}
 	
+	/** Creates a new instance of the item.
+	 * @return HealingItem
+	 */
 	public HealingItem copy() {return new HealingItem(this.getName(), this.getDescription(), this.getPrice(), healingAmount);}
+	
+	/** Get the healing amount of the item
+	 * @return
+	 */
 	public int getHealingAmount() {return this.healingAmount;}
 	
 }
 
-//package items;
-//
-//import characters.Hero;
-//
-//public abstract class HealingItem extends Item {
-//	
-//	private int healingAmount;
-//	private int healingTime;
-//	
-//	// Constructor
-//	
-//	public HealingItem(String name, String description, int price, int healingAmount, int healingTime) {
-//		super(name, description, price);
-//		this.name = name;
-//		this.description = description;
-//		this.price = price;
-//		this.healingAmount = healingAmount;
-//		this.healingTime = healingTime;
-//	}
-//	
-////	public abstract boolean useItem(Hero hero);
-//	
-//	public int getHealingAmount() { return this.healingAmount; }
-//	
-//	public int getHealingTime() {	return this.healingTime;	}
-//	
-//	public void setHealingAmount(int healingAmount) {   this.healingAmount = healingAmount;   } 
-//
-//	public void setHealingTime(int healingTime) {	this.healingTime = healingTime;	}
-//	
-//	public String toString() {
-//		return name;
-//	}
-//}
