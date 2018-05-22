70fcc25924ae9a795341e5425e130e937e80eb9b
hannahregan <38155691+hannahregan@users.noreply.github.com>
2018-05-22 17:53:25 +1200

    Update PowerupDodge.java

--- items/PowerupDodge.java
+++ items/PowerupDodge.java
@@ -1,10 +1,29 @@
 package items;
 
+/**
+ * This class creates the class 'PowerupDodge', a subclass of Powerup, and can be stored in the Team's array list, powerupsInventory
+ * 
+ * @author Harrison Cook
+ * @author Hannah Regan
+ * @version 0.1 04/04/2018
+ */
+
 public class PowerupDodge extends Powerup {
+	
+	/**
+	 * Constructor
+	 * @param name The name of the Power Up.
+	 * @param description The description of the Power Up
+	 * @param price How much the Power Up costs to purchase
+	 */
 	public PowerupDodge() {
         super("Ninja Tabi", "Chance of dodging a villain's attack", 60);
     }
 
+	/** 
+	 * Creates a new instance of the Power Up.
+	 * @return PowerUpDamage
+	 */
 	@Override
 	public PowerupDodge copy() {return new PowerupDodge();}
 }
