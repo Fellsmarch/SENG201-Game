60789caf0a9878f396f2fa2b77f6fd828eab21a2
hannahregan <38155691+hannahregan@users.noreply.github.com>
2018-05-25 00:21:02 +1200

    Update SecondWind.java

--- characters/HeroTypes/SecondWind.java
+++ characters/HeroTypes/SecondWind.java
@@ -12,14 +12,24 @@ import characters.Hero;
 
 
 public class SecondWind extends Hero
-	{
+	{	
+		/**
+		 * Sets hasDied to false 
+		 */
 		private boolean hasDied = false;
 		
+		/**
+		 * Constructor -- Creates a SecondWind Hero
+		 * @param name The name of the Hero
+		 */
 		public SecondWind(String name) {
 			super(name);
 			heroType = "Darkin";
 		}
 		
+		/**
+		 * Determines whether the Hero's health <= 0, if so, sets the current health to 1 and gives the Hero "another chance"
+		 */
 		@Override
 		public boolean death() {
 			if (hasDied) {
@@ -31,11 +41,10 @@ public class SecondWind extends Hero
 			}
 		}
 		
-//		public String toString() {
-//			String old = super.toString();
-//			return old + "When " + name + " dies, they come back to life with 1 hp. This can only happen once\n";
-//		}
-		
+		/**
+		 * Gets all the Hero's attributes to display as a printed String
+		 * @return A string that represents all of the Hero's stats/attributes, alongside a description of the Hero's type
+		 */
 		@Override
 		public String toString(boolean userHero) {
 			return super.toString() + " - If this hero were to die, they stay alive with 1 health. This can only happen once";
