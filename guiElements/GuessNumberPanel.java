d7d076e40253b3ce1cdf3eccc9e07e9e7ce2e6ae
hannahregan <38155691+hannahregan@users.noreply.github.com>
2018-05-25 02:58:08 +1200

    Update GuessNumberPanel.java

--- guiElements/GuessNumberPanel.java
+++ guiElements/GuessNumberPanel.java
@@ -14,66 +14,108 @@ import net.miginfocom.swing.MigLayout;
 import javax.swing.JToggleButton;
 import javax.swing.UIManager;
 
+/**
+ * This class creates the class 'GuessNumberPanel', extends GamePanel and creates a panel for the Guess Number game
+ * 
+ * @author Harrison Cook
+ * @author Hannah Regan
+ * @version 0.1 04/04/2018
+ */
 @SuppressWarnings("serial")
 public class GuessNumberPanel extends GamePanel {
 	
+	/**
+	 * Boolean morelucky;
+	 */
 	private boolean moreLucky;
+	
+	/**
+	 * Villain that will verse the chosen Hero
+	 */
 	private Villain villain;
+	
+	/**
+	 * JProgressBar for results of who won the game
+	 */
 	private JProgressBar resultDisplay;
+	
+	/**
+	 * Number of guesses that the user has had
+	 */
 	private int guessNum = 0;
+	
+	/**
+	 * Array of buttons that can be selected and unselected
+	 */
 	private JToggleButton[] numberButtons;
-	private int villainsNumber, userGuess;
+	
+	/**
+	 * Number that the villain "chooses"
+	 */
+	private int villainsNumber;
+	
+	/**
+	 * User's number choice
+	 */
+	private int userGuess;
 
 	/**
-	 * Create the panel.
+	 * Constructor -- Create the panel
+	 * @param challenger, Hero that is chosen by user to play the game
+	 * @param villain, villain that will challenge the Hero
+	 * @param moreLucky, boolean 
 	 */
 	public GuessNumberPanel(Hero challenger, Villain villain, boolean moreLucky) {
 		this.moreLucky = moreLucky;
 		this.villain = villain;
 		Random rand = new Random();
 		villainsNumber = rand.nextInt(10) + 1;
-		setLayout(new MigLayout("", "[grow][grow][grow]", "[50px:75px][50px:75px][50px:75px][50px:75px][]"));
+		setLayout(new MigLayout("", "[grow][grow][grow][grow]", "[grow][grow][grow][grow]"));
+		
+		UIManager.put("ProgressBar.selectionForeground", Color.BLACK);
+		UIManager.put("ProgressBar.selectionBackground", Color.BLACK);
 		
 		JToggleButton tgl1 = new JToggleButton("1"); JToggleButton tgl2 = new JToggleButton("2"); JToggleButton tgl3 = new JToggleButton("3");
 		JToggleButton tgl4 = new JToggleButton("4"); JToggleButton tgl5 = new JToggleButton("5"); JToggleButton tgl6 = new JToggleButton("6");
 		JToggleButton tgl7 = new JToggleButton("7"); JToggleButton tgl8 = new JToggleButton("8"); JToggleButton tgl9 = new JToggleButton("9");
 		JToggleButton tgl10 = new JToggleButton("10");
 		
-		add(tgl1, "flowy,cell 0 0,grow"); add(tgl2, "flowy,cell 1 0,grow"); add(tgl3, "flowy,cell 2 0,grow"); add(tgl4, "cell 0 1,grow");
+		add(tgl1, "flowy,cell 0 0,grow"); add(tgl2, "flowy,cell 1 0,grow"); add(tgl3, "flowy,cell 2 0,grow"); 
+		add(tgl4, "cell 0 1,grow");
 		add(tgl5, "cell 1 1,grow"); add(tgl6, "cell 2 1,grow"); add(tgl7, "cell 0 2,grow"); add(tgl8, "cell 1 2,grow"); 
-		add(tgl9, "cell 2 2,grow"); add(tgl10, "cell 1 3,grow");
+		add(tgl9, "cell 2 2,grow"); add(tgl10, "cell 3 2,grow");
+		 resultDisplay= new JProgressBar();
+		 resultDisplay.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		 resultDisplay.setStringPainted(true);
+		 resultDisplay.setForeground(Color.YELLOW);
+		 resultDisplay.setString("Result");
+		 resultDisplay.setValue(100);
+		 add(resultDisplay, "cell 0 3 4 1,grow");
 		
 		JToggleButton[] buttons = {tgl1, tgl2, tgl3, tgl4, tgl5, tgl6, tgl7, tgl8, tgl9, tgl10};
 		numberButtons = buttons;
 		
 		ButtonGroup numChoice = new ButtonGroup();
 		for (JToggleButton button : buttons) {numChoice.add(button);}
-//		numChoice.add(tgl1); numChoice.add(tgl2); numChoice.add(tgl3); numChoice.add(tgl4); numChoice.add(tgl5); 
-//		numChoice.add(tgl6); numChoice.add(tgl7); numChoice.add(tgl8); numChoice.add(tgl9);
-		
-		UIManager.put("ProgressBar.selectionForeground", Color.BLACK);
-		UIManager.put("ProgressBar.selectionBackground", Color.BLACK);
-		
-		resultDisplay= new JProgressBar();
-		resultDisplay.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		resultDisplay.setStringPainted(true);
-		resultDisplay.setForeground(Color.YELLOW);
-		resultDisplay.setString("Result");
-		resultDisplay.setValue(100);
-		add(resultDisplay, "cell 0 4 3 1,grow");
+
 		
 	}
 
+	/**
+	 * Plays the game 
+	 * @return the result of the game (who won)
+	 */
 	@Override
 	public Result playGame() {
 		for (JToggleButton button : numberButtons) {
 			if (button.isSelected()) {userGuess = Integer.parseInt(button.getText()); break;}
 		}
+		if (userGuess == 0) {return Result.DRAW;}
 		if (userGuess == villainsNumber) {return Result.WIN;}
 		else if (userGuess > villainsNumber) {//Lower
 			if (moreLucky && userGuess - 1 == villainsNumber) {return Result.WIN;}
 			else if (guessNum < 1){
-				if (userGuess != 0) {resultDisplay.setString("Lower! - One guess left");}
+				resultDisplay.setString("Lower! - One guess left");
 				guessNum++;
 				return Result.DRAW;
 			} else {return Result.LOSS;}
@@ -86,12 +128,13 @@ public class GuessNumberPanel extends GamePanel {
 			} else {return Result.LOSS;}
 		}
 	}
-
+	/**
+	 * Displays the result (who won)
+	 */
 	@Override
 	public void displayResult(Result result) {
 		if (userGuess == 0) {
 			JOptionPane.showMessageDialog(null, "Please select a number!", "Make Choice", JOptionPane.WARNING_MESSAGE);
-			guessNum = 0;
 		}
 		else {
 			if (result == Result.WIN) {
@@ -103,7 +146,11 @@ public class GuessNumberPanel extends GamePanel {
 			}
 		}
 	}
-
+	
+	/**
+	 * toString() for the Guess Number game
+	 * @return a String representation of the Guess Number game
+	 */
 	@Override
-	public String toString() {return "Guess " + villain.getName() + "'s Number Game";}
+	public String toString() {return "Guess " + villain.getName() + "'s Number";}
 }
