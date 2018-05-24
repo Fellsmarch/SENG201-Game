50a8e61008d7c75bd9b9f17668c84ab858e828f0
hannahregan <38155691+hannahregan@users.noreply.github.com>
2018-05-25 04:32:50 +1200

    Update TeamCreationPanel.java

--- guiElements/TeamCreationPanel.java
+++ guiElements/TeamCreationPanel.java
@@ -3,7 +3,6 @@ import javax.swing.JPanel;
 import net.miginfocom.swing.MigLayout;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
-import javax.swing.JTextField;
 
 import java.util.ArrayList;
 
@@ -20,32 +19,68 @@ import characters.HeroType;
 import javax.swing.JSeparator;
 import java.awt.Color;
 import javax.swing.SwingConstants;
-import javax.swing.JButton;
 
+/**
+ * This class creates the class 'TeamCreationPanel', extends JPanel, and contains the operations regarding the creation of the Team
+ * 
+ * @author Harrison Cook
+ * @author Hannah Regan
+ * @version 0.1 04/04/2018
+ */
 @SuppressWarnings("serial")
 public class TeamCreationPanel extends JPanel
 	{
+	
+	/**
+	 * Array list of heroes to be stored as the team
+	 */
 	private ArrayList<Hero> heroes = new ArrayList<Hero>();
+	
+	/**
+	 * JTextPane that displays the hero's stats
+	 */
 	private JTextPane paneDisplayHeroStats;
+	
+	/**
+	 * JLabel for the second hero, if chosen
+	 */
 	private JLabel lblSecondHero;
+	
+	/**
+	 * JLabel for the third hero, if chosen
+	 */
 	private JLabel lblThirdHero;
+	
+	/**
+	 * JLabel for the first hero
+	 */
 	private JLabel lblFirstHero = new JLabel("Your Hero:");
+	
+	/**
+	 * Array of create hero panels
+	 */
 	private CreateHeroPanel[] createHeroPanels;
+	
+	/**
+	 * Position on the layout
+	 */
 	private int position;
 
 		/**
-		 * Create the panel.
+		 * Constructor -- Create the panel
+		 * @param numHeroes, number of chosen heroes
 		 */
 		public TeamCreationPanel(int numHeroes)
 			{
-				String rows = "[][][][grow]";
+				String rows = "[center][center][center][grow,center]";
 				if (numHeroes > 1) {rows = "[][]" + rows;}
 				if (numHeroes > 2) {rows = "[][]" + rows;}
 				position = (numHeroes - 1) * 2;
-				setLayout(new MigLayout("", "[][][]", rows));
+				setLayout(new MigLayout("", "[grow][][grow]", rows));
 				
 				add(lblFirstHero, "cell 0 0 3 1");
 				
+				CreateHeroPanel.resetHeroNames();
 				createHeroPanels = new CreateHeroPanel[numHeroes];
 				int panelPos = 1;
 				for (int i = 0; i < numHeroes; i++) {
@@ -63,18 +98,11 @@ public class TeamCreationPanel extends JPanel
 					add(lblThirdHero, "cell 0 4 3 1");
 					}
 	
-	//			add(new CreateHeroPanel(), "cell 0 1 3 1,growx");
-	//			add(new CreateHeroPanel(), "cell 0 3 3 1,growx");
-	//			add(new CreateHeroPanel(), "cell 0 5 3 1,growx");
-				
-				JComboBox<HeroType> comboBox = new JComboBox<HeroType>();
-				comboBox.addActionListener(new ActionListener() {
+				JComboBox<HeroType> comboHeroSelector = new JComboBox<HeroType>();
+				comboHeroSelector.addActionListener(new ActionListener() {
 					public void actionPerformed(ActionEvent e) {
-						Hero hero = ((HeroType) comboBox.getSelectedItem()).createHero(comboBox.getSelectedItem().toString());
+						Hero hero = ((HeroType) comboHeroSelector.getSelectedItem()).createHero(comboHeroSelector.getSelectedItem().toString());
 						paneDisplayHeroStats.setText(hero.toString(false));
-//						if (hero instanceof RandomHero) 
-//							 {textPane.setText(((RandomHero) hero).toString(false));}
-//						else {textPane.setText(hero.toString());}
 					}
 				});
 				
@@ -85,13 +113,13 @@ public class TeamCreationPanel extends JPanel
 				
 				JLabel lblViewStats = new JLabel("View stats for hero type:");
 				add(lblViewStats, "flowy,cell 0 " + (3 + position) + ",alignx center,aligny top");
-				comboBox.setModel(new DefaultComboBoxModel<HeroType>(HeroType.values()));
-				add(comboBox, "cell 0 " + (3 + position) + ",aligny top");
+				comboHeroSelector.setModel(new DefaultComboBoxModel<HeroType>(HeroType.values()));
+				add(comboHeroSelector, "cell 0 " + (3 + position) + ",growx,aligny top");
 				
 				paneDisplayHeroStats = new JTextPane();
 				paneDisplayHeroStats.setBackground(UIManager.getColor("this.background"));
 				paneDisplayHeroStats.setContentType("text/html");
-				paneDisplayHeroStats.setText(((HeroType) comboBox.getSelectedItem()).createHero(comboBox.getSelectedItem().toString()).toString(false));
+				paneDisplayHeroStats.setText(((HeroType) comboHeroSelector.getSelectedItem()).createHero(comboHeroSelector.getSelectedItem().toString()).toString(false));
 				paneDisplayHeroStats.setEditable(false);
 				add(paneDisplayHeroStats, "cell 2 " + (3 + position) + ",aligny top");
 				
@@ -100,32 +128,12 @@ public class TeamCreationPanel extends JPanel
 				separator.setForeground(Color.BLACK);
 				separator.setBackground(Color.BLACK);
 				add(separator, "cell 1 " + (3 + position) + ",alignx right,growy");
-				
-//				JButton btnContinue = new JButton("Continue");
-//				btnContinue.addActionListener(new ActionListener() {
-//					public void actionPerformed(ActionEvent e) {
-//						System.out.println("Button Pressed");
-//						boolean allInputGood = true;
-//						for (CreateHeroPanel panel : createHeroPanels) {
-//							System.out.println("In loop");
-//							if (panel.heroCreated()) {
-//								System.out.println("Hero Created");
-//								heroes.add(panel.getHero());
-//							} else {
-//								JOptionPane.showMessageDialog(null, "Cannot continue until all heroes are submitted!", "Heroes not submitted", JOptionPane.ERROR_MESSAGE);
-//								heroes = new ArrayList<Hero>();
-//								allInputGood = false;
-//								break;
-//							}
-//						}
-//						if (allInputGood) {
-//							String s = "Continue";
-//						}
-//					}
-//				});
-//				add(btnContinue, "cell 0 " + (3 + position) + ",growx,height 50:70:70");
 			}
 		
+			/**
+			 * Checks to see whether all the heroes have been submitted
+			 * @return allInputGood if the user has entered valid input and can progress through to the next stage of the game
+			 */
 			public boolean readyToContinue() {
 				boolean allInputGood = true;
 				for (CreateHeroPanel panel : createHeroPanels) {
@@ -141,6 +149,10 @@ public class TeamCreationPanel extends JPanel
 				return allInputGood;
 			}
 			
+			/**
+			 * Gets the array list of heroes
+			 * @return
+			 */
 			public ArrayList<Hero> getHeroes() {
 				return heroes;
 			}
