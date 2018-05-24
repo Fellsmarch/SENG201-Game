f58a06cc5e8176b2bc7edcacf0708e1b17f5befc
hannahregan <38155691+hannahregan@users.noreply.github.com>
2018-05-25 00:23:26 +1200

    Update Villain.java

--- characters/Villain.java
+++ characters/Villain.java
@@ -2,51 +2,120 @@ package characters;
 
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Random;
 
-import commandLineElements.Game;
+import guiElements.GameType;
 
+/**
+ * This class creates a superclass "Villain", an object that stores all the relevant variables and methods that a "Villain" would possess
+ * 
+ * @author Harrison Cook
+ * @author Hannah Regan
+ * @version 0.1 04/04/2018
+ */
 public class Villain
 	{
+		/**
+		 * The Villain's name
+		 */
 		private String name;
+		
+		/**
+		 * The Villain's title
+		 */
 		private String title;
-		private ArrayList<Game> gameList;					//Could change this to array, it would make it easier to create it in the game environment
-															//private ArrayList<String> gameList; used for testing without a list of games
-		private Game favouriteGame;
-															//private String favouriteGame;
-		private double favouriteGameChance; //Could be int
+		
+		/**
+		 * An array of game types that the Villain could challenge the Hero with
+		 */
+		private ArrayList<GameType> gameList = new ArrayList<GameType>(Arrays.asList(GameType.values()));
+		
+		/**
+		 * The game most likely to be played by this Villain (Villain's "favourite" game)
+		 */
+		private GameType favouriteGame;
+		
+		/**
+		 * Chance that the Villain's favourite game will be picked from the gameList
+		 */
+		private double favouriteGameChance; 
+		
+		/**
+		 * The Villain's taunt phrase
+		 */
 		private String tauntPhrase;
+		
+		/**
+		 * The damage that the Villain deals to a Hero
+		 */
 		protected int damage;
+		
+		/**
+		 * The Villain's health
+		 */
 		protected int health;
+		
+		/**
+		 * The amount of money received by the Team if the Team defeats the Villain
+		 */
 		protected int killReward;
 		
-		public Villain(String[] nameTitle, String taunt, ArrayList<Game> games) { 
+		/** Creates a Villain
+		 * @param nameTitle The Villain's name and title from an array
+		 * @param taunt The Villain's taunt phrase
+		 */
+		public Villain(String[] nameTitle, String taunt) { 
 			name = nameTitle[0];
 			title = nameTitle[1];
 			tauntPhrase = taunt;
-			gameList = games;
 			
-			//Getting random favourite game and favourite game chance
+			/**
+			 * Assigns the size of the array list, gameList, to a variable gameListSize
+			 */
 			int gameListSize = gameList.size();
+			
+			/**
+			 * Creates the randomiser
+			 */
 			Random randomiser = new Random();
+			
+			/**
+			 * Uses the randomiser to assign a favourite game to a Villain
+			 */
 			int favGameIndex = randomiser.nextInt(gameListSize);
 			favouriteGame = gameList.get(favGameIndex);
+			
+			/**
+			 * Uses the randomiser to assign the chance that the favourite game will be picked
+			 * This will mean that the number will be between probOfGame and 100
+			 */
 			int probOfGame = 100 / gameListSize;
-			favouriteGameChance = randomiser.nextInt(100 - probOfGame) + probOfGame ; 	//Doing it this way means the number will be between probOfGame and 100
-																						//System.out.println(favouriteGameChance);
+			favouriteGameChance = randomiser.nextInt(100 - probOfGame) + probOfGame ; 	
 			
-			//Randomising stats --> these will need to be changed when actual values are known
-			damage = randomiser.nextInt(20) + 20; //Numbers are placeholders at the moment
-			health = randomiser.nextInt(300) + 100; //Numbers are placeholders at the moment
-			killReward = randomiser.nextInt(500) + 50; //Numbers are placeholders at the moment
+			/**
+			 * Randomises the damage dealt by the Villain
+			 */
+			damage = randomiser.nextInt(15) + 30; 
 			
+			/**
+			 * Randomises the health of the Villain
+			 */
+			health = randomiser.nextInt(50) + 100; 
 			
+			/**
+			 * Randomises the kill reward that the Villain grants the Team
+			 */
+			killReward = randomiser.nextInt(200) + 50; 
 		}
 		
-		public Game chooseGame() {
+		/**
+		 * Uses the randomiser to choose the game to be played by the Villain
+		 * @return Game that will be played 
+		 */
+		public GameType chooseGame() {
 			Random randomiser = new Random();
 			int toTest = randomiser.nextInt(100) + 1;
-																						//System.out.println(toTest);
 			if(toTest <= favouriteGameChance) {
 				return favouriteGame;
 			}else {
@@ -55,24 +124,49 @@ public class Villain
 				gameList.add(favouriteGame);
 				return gameList.get(gameIndex);
 			}
-			
 		}
-		
+		/**
+		 * Gets the Villain's name
+		 * @return
+		 */
 		public String getName() {return name;}
-		public String getTitle() {return title;}
-		public String getTaunt() {return tauntPhrase;}
-		public int getDamage() {return damage;}
-		public int getHealth() {return health;}
-		public int getReward() {return killReward;}
-		public void removeHealth(int healthLost) {health -= healthLost;}
-	
 		
-//		public static void main(String[] args) { //For testing only
-//			ArrayList<String> games = new ArrayList<String>();
-//			games.add("PSR"); games.add("Dice game"); games.add("Guess number");
-//			Villain testVillain = new Villain("Hannah", "I am the second-best member of the team!... ;)", games);
-//			System.out.println("Favourite game: " + testVillain.favouriteGame);
-//			System.out.println(testVillain.chooseGame());
-//			
-//		}
+		/**
+		 * Gets the Villain's title
+		 * @return
+		 */
+		public String getTitle() {return title;}
+		
+		/**
+		 * Gets the Villain's taunt phrase
+		 * @return
+		 */
+		public String getTaunt() {return tauntPhrase;}
+		
+		/**
+		 * Gets the damage done by the Villain
+		 * @return
+		 */
+		public int getDamage() {return damage;}
+		
+		/**
+		 * Gets the Villain's health
+		 * @return
+		 */
+		public int getHealth() {return health;}
+		
+		/**
+		 * Gets the amount of money received by the team if the Villain is defeated
+		 * @return
+		 */
+		public int getReward() {return killReward;}
+		
+		/**
+		 * Removes the Villain's health if damage is dealt and health is lost
+		 * @param healthLost The health lost from the Villain is damage is dealt by the Hero
+		 */
+		public void removeHealth(int healthLost) {health -= healthLost;}
+		
+		
+	
 	}
