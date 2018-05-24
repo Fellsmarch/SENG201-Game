416e4db44df699c250dbe8e7768bad060766046c
hannahregan <38155691+hannahregan@users.noreply.github.com>
2018-05-25 00:22:50 +1200

    Update SuperVillain.java

--- characters/SuperVillain.java
+++ characters/SuperVillain.java
@@ -1,16 +1,26 @@
 package characters;
-import java.util.ArrayList;
 import java.util.Random;
 
-import commandLineElements.Game;
-
-public class SuperVillain extends Villain
-	{
-		public SuperVillain(String[] nameTitle, String taunt, ArrayList<Game> games) {
-			super(nameTitle, taunt, games);
-			Random rand = new Random();
-			damage = rand.nextInt(40) + 20;			//Numbers are placeholders
-			health = rand.nextInt(1000) + 500;		//Numbers are placeholders
-			killReward = 1000000;					//Numbers are placeholders
-		}
+/**
+ * This class is a subclass of the superclass "Villain", is a "stronger" Villain (has better stats)
+ * 
+ * @author Harrison Cook
+ * @author Hannah Regan
+ * @version 0.1 04/04/2018
+ */
+public class SuperVillain extends Villain {
+	
+	/**
+	 * Constructor
+	 * @param name The name of the Power Up.
+	 * @param description The description of the Power Up
+	 * @param price How much the Power Up costs to purchase
+	 */
+	public SuperVillain(String[] nameTitle, String taunt) {
+		super(nameTitle, taunt);
+		Random rand = new Random();
+		damage = rand.nextInt(40) + 20;			//Numbers are placeholders
+		health = rand.nextInt(150) + 150;		//Numbers are placeholders
+		killReward = 1000000;					//Numbers are placeholders
 	}
+}
