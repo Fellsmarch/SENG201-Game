d0f797e997f5aed5f7389d04276f188ecba46321
hannahregan <38155691+hannahregan@users.noreply.github.com>
2018-05-25 00:20:43 +1200

    Update RandomHero.java

--- characters/HeroTypes/RandomHero.java
+++ characters/HeroTypes/RandomHero.java
@@ -1,4 +1,9 @@
 package characters.HeroTypes;
+
+import java.util.Random;
+
+import characters.Hero;
+
 /**
  * This hero type randomises many of a Hero's stats, it randomises:
  * Health 					(between 1-200)
@@ -12,61 +17,38 @@ package characters.HeroTypes;
  * @author Hannah Regan
  * @version 0.1 04/04/2018
  */
-
-import java.util.Random;
-
-import characters.Hero;
-
 public class RandomHero extends Hero
 	{
-//		private boolean userHero = true;
+		/**
+		 * Creates a new randomiser
+		 */
 		private Random randomiser = new Random();
 		
+		/**
+		 * Constructor -- Creates a RandomHero
+		 * @param name The name of the Hero
+		 */
 		public RandomHero(String name) {
 			super(name);
 			int health = randomiser.nextInt(200) + 1;
 			maxHealth = health; currentHealth = health;
-			recoveryRate = randomiser.nextInt(11);
-			double newPrice = randomiser.nextInt(11) + 5; shopPrice = newPrice / 10; //randomiser.nextDouble() generates a random number between 0.0-1.0, should change these to this
+			recoveryRate = randomiser.nextInt(10) + 1;
+			double newPrice = randomiser.nextInt(11) + 5; shopMod = newPrice / 10; //randomiser.nextDouble() generates a random number between 0.0-1.0, should change these to this
 			double newAttack = randomiser.nextInt(20) + 1; attackMod = newAttack / 10;
 			double newDefense = randomiser.nextInt(20) + 1; defenseMod = newDefense / 10;
 			double newLoot = randomiser.nextInt(11) + 5; lootMod = newLoot / 10;
 			heroType = "ARAM";
 		}
 		
-//		public RandomHero(String name, boolean userHero) {
-//			super(name);
-//			this.userHero = userHero;
-//			int health = randomiser.nextInt(200) + 1;
-//			maxHealth = health; currentHealth = health;
-//			recoveryRate = randomiser.nextInt(11);
-//			double newPrice = randomiser.nextInt(11) + 5; shopPrice = newPrice / 10; //randomiser.nextDouble() generates a random number between 0.0-1.0, should change these to this
-//			double newAttack = randomiser.nextInt(20) + 1; attackMod = newAttack / 10;
-//			double newDefense = randomiser.nextInt(20) + 1; defenseMod = newDefense / 10;
-//			double newLoot = randomiser.nextInt(11) + 5; lootMod = newLoot / 10;
-//		}
-		
-//		public String toString() {
-//			return super.toString();
-//		}
-		
+		/**
+		 * Gets all the Hero's attributes to display as a printed String
+		 * @return A string that represents all of the Hero's stats/attributes (in this case - "???") 
+		 */
 		@Override
 		public String toString(boolean userHero) {
-//			if (userHero) {return super.toString();} 
-//			else {
+
 				return  "<b>" + name + 
 						"</b>:<br />Health:&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;???/???<br />Recovery Rate:&emsp;&emsp;???<br />Attack Strength:&emsp;&nbsp;???<br />Defense Modifier:&ensp;???<br />Shop Price:&emsp;&emsp;&emsp;&ensp;&nbsp;???<br />Loot Modifier:&emsp;&emsp;&ensp;???<br />??????????????????????</html>";
-//				} "<html><font face=\"Dialog\" size=\"4\"><b>"
 		}
-		
-//		public static void main(String[] args) {
-//			RandomHero test = new RandomHero("Jim");
-//			RandomHero test2 = new RandomHero("Tim", false);
-////			System.out.println(test.getHealth());
-////			System.out.println(test.getRecovery());
-////			System.out.println(test.getShopPrice());
-//			System.out.println(test);
-//			System.out.println(test2);
-//			
-//		}
+
 	}
