19fdfcf2f98da6e3aff9fcd87e4d458e52ae9e37
hannahregan <38155691+hannahregan@users.noreply.github.com>
2018-05-25 03:48:28 +1200

    Update Result.java

--- guiElements/Result.java
+++ guiElements/Result.java
@@ -1,5 +1,12 @@
 package guiElements;
 
+/**
+ * This enum states the possible outcomes of a game played by a user versus a villain
+ * 
+ * @author Harrison Cook
+ * @author Hannah Regan
+ * @version 0.1 04/04/2018
+ */
 public enum Result {
 	WIN,
 	DRAW,
