3bc6d346204fc7f7b79e678b09c2f601031ae874
hannahregan <38155691+hannahregan@users.noreply.github.com>
2018-05-25 00:19:50 +1200

    Update DiscountShopper.java

--- characters/HeroTypes/DiscountShopper.java
+++ characters/HeroTypes/DiscountShopper.java
@@ -13,10 +13,14 @@ public class DiscountShopper extends Hero
 	{
 		public DiscountShopper(String name) {
 			super(name);
-			shopPrice = 0.5;
+			shopMod = 0.5;
 			heroType = "Storedle";
 		}
 		
+		/**
+		 * Gets all the Hero's attributes to display as a printed String
+		 * @return A string that represents all of the Hero's stats/attributes, alongside a description of the Hero's type
+		 */
 		@Override
 		public String toString(boolean userHero) {
 			return super.toString() + " - This hero halves the cost of items in the shop whilst they are in your team";
