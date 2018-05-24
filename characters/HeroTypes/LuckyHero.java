ff1d17c8d33265335b129cc9d9c45dee1d3e257a
hannahregan <38155691+hannahregan@users.noreply.github.com>
2018-05-25 00:20:17 +1200

    Update LuckyHero.java

--- characters/HeroTypes/LuckyHero.java
+++ characters/HeroTypes/LuckyHero.java
@@ -12,16 +12,21 @@ import characters.Hero;
 
 public class LuckyHero extends Hero
 	{
-		public LuckyHero(String name) { //Chance of getting good events vs bad ones is increased
+	
+	/** 
+	 * Constructor -- Creates a CEOHero
+	 * @param name The name of the hero
+	 */
+		public LuckyHero(String name) { 
 			super(name);
 			eventChance = 0.25;
 			heroType = "Card Master";
 		}
 		
-//		public String toString() {
-//			String old = super.toString();
-//			return old + name + " increases the chance of the team getting a free item instead of being robbed\n";
-//		}
+		/**
+		 * Gets all the Hero's attributes to display as a printed String
+		 * @return A string that represents all of the Hero's stats/attributes, alongside a description of the Hero's type
+		 */
 		@Override
 		public String toString(boolean userHero) {
 			return super.toString() + " - This hero increases the chance of getting free items randomly whilst they are in your team";
