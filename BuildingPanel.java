d15b96742cf45e45ac91686bd39b7eaad8996623
Fellsmarch <37695784+Fellsmarch@users.noreply.github.com>
2018-05-25 00:54:36 +1200

    Delete BuildingPanel.java

--- BuildingPanel.java
+++
@@ -1,9 +0,0 @@
-package guiElements;
-
-import javax.swing.JPanel;
-
-@SuppressWarnings("serial")
-public abstract class BuildingPanel extends JPanel {
-	public abstract String toString();
-	public abstract void updateDisplays();
-}
